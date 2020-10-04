61b074c22c79ee032f69c142a7aacf57252217a6
Saurabh <saurabhbraryo@gmail.com>
2020-10-05 01:34:01 +0530

    added room creation only for logged in users

--- src/components/mainpage/mainpage.js
+++ src/components/mainpage/mainpage.js
@@ -1,18 +1,23 @@
 import React, { useState, useEffect, useRef } from 'react';
 import {
     BrowserRouter as Router,
-    Link
+    Link,
+    useHistory
 } from "react-router-dom";
 import './mainpage.css'
 import codeShareGIF from './coded-shareGIF.gif'
 import { TimelineLite, TweenMax, Power3 } from 'gsap';
+import jwt_decode from "jwt-decode";
+import { logoutUser } from "../../actions/authActions";
 
 function MainPage(props) {
     const socket = props.socket
     const char_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     const length = 6
     const [code, setCode] = useState("")
-
+    const [name, setName] = useState("")
+    const [render, setRender] = useState(true)
+    const history = useHistory();
     let app = useRef(null)
     let content = useRef(null)
     let image = useRef(null)
@@ -22,7 +27,15 @@ function MainPage(props) {
     let headlineThird = useRef(null)
     let tl = new TimelineLite({ delay: .4 });
 
-
+    if (render == true) {
+        const token = localStorage.getItem('jwtToken');
+        if (token) {
+            const decoded = jwt_decode(token);
+            console.log(decoded)
+            setName(decoded.name)
+            setRender(false)
+        }
+    }
 
     const generateCode = () => {
         let text = ""
@@ -34,13 +47,19 @@ function MainPage(props) {
 
     useEffect(() => {
         if (code != "") {
-            socket.emit('created-room', code)
-            console.log('CREATED-ROOM')
+            if (name == "") {
+                history.push(`/login`)
+            } else {
+                socket.emit('created-room', code)
+                console.log('CREATED-ROOM')
+                history.push(`/editor/${code}`)
+            }
         }
 
     }, [code])
 
     useEffect(() => {
+
         //content vars
         // const headlineFirst = content.children[0].children[0];
         // const headlineSecond = headlineFirst.nextSibling;
@@ -51,11 +70,11 @@ function MainPage(props) {
         //Remove initial flash
         TweenMax.to(app, 0, { css: { visibility: 'visible' } })
         // TweenMax.to(button, 5.5, { css: { visibility: 'visible' } })
-        TweenMax.to(button, 1, { opacity:1, delay:3 });
-        TweenMax.to(headlineThird, 1, { opacity:1, delay:2 });
+        TweenMax.to(button, 1, { opacity: 1, delay: 3 });
+        TweenMax.to(headlineThird, 1, { opacity: 1, delay: 2 });
 
 
-        tl.from(image, 0.6, {y: 1280, ease: Power3.easeOut},'Start')
+        tl.from(image, 0.6, { y: 1280, ease: Power3.easeOut }, 'Start')
 
         // Content Animation
         tl.staggerFrom([headlineSecond], 1, {
@@ -64,7 +83,7 @@ function MainPage(props) {
             delay: .4
         }, .15, 'Start')
             .from(contentP, 1, { y: 40, opacity: 0, ease: Power3.easeOut }, 0.8)
-            // .from(contentButton, 1, { y: 20, opacity: 0, ease: Power3.easeOut }, 1.6)
+        // .from(contentButton, 1, { y: 20, opacity: 0, ease: Power3.easeOut }, 1.6)
 
     }, [tl])
 
@@ -72,14 +91,24 @@ function MainPage(props) {
         pathname: `/editor/${code}`,
         state: code
     };
+
+    const onLogoutClick = (e) => {
+        localStorage.removeItem("jwtToken");
+        window.location.reload();
+    };
+
     return (
         <div className="main" ref={el => app = el}>
             <nav class="navbar">
                 <Link to="/" className="logo">CodeLive</Link>
                 <ul class="nav-links">
-
-                    <li class="nav-item link"><Link to="/register">Sign up</Link></li>
-                    <li class="nav-item login link "><Link to="/login">Log in</Link></li>
+                    {name != "" ?
+                        <><li class="nav-item link"><Link to="/register">Hello, {name}</Link></li>
+                            <li class="nav-item login link "><Link to="/" onClick={onLogoutClick}>Log out</Link></li></ > :
+                        <><li class="nav-item link"><Link to="/register">Sign up</Link></li>
+                            <li class="nav-item login link "><Link to="/login">Log in</Link></li></ >}
+                    {/* <li class="nav-item link"><Link to="/register">Sign up</Link></li>
+                    <li class="nav-item login link "><Link to="/login">Log in</Link></li> */}
                 </ul>
             </nav>
 
@@ -93,7 +122,7 @@ function MainPage(props) {
                     </div>
 
                     <div className="container-flex-intro-video">
-                        <img src={codeShareGIF} className="codeShareGIF" ref={el => image = el}/>
+                        <img src={codeShareGIF} className="codeShareGIF" ref={el => image = el} />
                     </div>
                     <div className="headlineThird container-flex-intro-continue" ref={el => headlineThird = el}>
                         Supports multiple languages with no limit on participants.
@@ -102,7 +131,7 @@ function MainPage(props) {
 
             </div>
             <div className="create-room ui text container" style={{ textAlign: "center" }} ref={el => button = el}>
-                <div onClick={generateCode}  
+                <div onClick={generateCode}
                     className="ui huge black button"
                     style={{ marginTop: "5vh", marginRight: "0em" }}
                 >
@@ -121,3 +150,13 @@ function MainPage(props) {
 }
 
 export default MainPage;
+
+
+    // useEffect(() => {
+    //     const token = localStorage.getItem('jwtToken');
+    //     if (token) {
+    //         const decoded = jwt_decode(token);
+    //         setName(decoded.name)
+    //     }
+
+    // }, [])
